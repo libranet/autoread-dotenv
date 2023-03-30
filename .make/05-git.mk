971a1ec6aa16216abd0181a356e95703e2cfd5cc
Wouter Vanden Hove <wouter@libranet.eu>
2023-03-30 11:00:34 +0200

    add code cov

--- .make/05-git.mk
+++ .make/05-git.mk
@@ -8,3 +8,27 @@ git-init:
 .PHONY: git-remote-show-origin  ## git-remote-show-origin
 git-remote-show-origin:
 	git remote show origin
+
+
+.PHONY: git-fetch-tags  ## git fetch --tags
+git-fetch-tags:
+	git fetch --tags
+
+
+.PHONY: git-show-remote-tags  ## git ls-remote --tags
+git-show-remote-tags:
+	git ls-remote --tags
+
+
+.PHONY: git-show-local-tags  ## git tag -l
+git-show-local-tags: git-fetch-tags
+	echo -e "Local tags"
+	git tag -l
+
+
+.PHONY: git-show-tags  ## show local & remote tags
+git-show-tags:
+	@echo -e "Local tags"
+	git tag -l
+	@echo -e "Remote tags"
+	git ls-remote --tags
