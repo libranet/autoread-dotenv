9d9f8fed487478bbed1586d28517f31ac378d7d1
Wouter Vanden Hove <wouter@libranet.eu>
2023-04-18 22:56:39 +0200

    add alias

--- .make/20-pytest.mk
+++ .make/20-pytest.mk
@@ -32,3 +32,7 @@ pytest-cov-pdb: pytest-pdb-cov
 .PHONY: pytest-pdb-cov-lf
 pytest-pdb-cov-lf: pytest-which
 	- pytest tests --cov=src --cov-report html -v --pdb --lf
+
+
+.PHONY: pytest-cov-pdb-lf  # alias for pytest-pdb-cov-lf
+	- pytest-cov-pdb-lf: pytest-pdb-cov-lf
