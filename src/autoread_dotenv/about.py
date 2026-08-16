b43e73521e79149b5c27cf3bb54b76e8adaf6938
Wouter Vanden Hove <wouter@libranet.eu>
2026-08-16 14:25:16 +0200

    simplify metadata

--- src/autoread_dotenv/about.py
+++ src/autoread_dotenv/about.py
@@ -9,22 +9,11 @@ from __future__ import annotations  # enables X | Y syntax in annotations for Py
 
 import importlib.metadata
 
+# by default __package__ is str|None
 PACKAGE: str = __package__ or ""
 
+pkginfo: importlib.metadata.PackageMetadata = importlib.metadata.metadata(PACKAGE)
 
-try:
-    msg = importlib.metadata.metadata(PACKAGE)
-    pkginfo: dict[str, str | list[str]] = msg.json  # type: ignore[attr-defined]
-except ValueError:  # pragma: no cover
-    # A distribution name is required. __package__ is None
-    pkginfo = {}
-except importlib.metadata.PackageNotFoundError:  # pragma: no cover
-    # fallback if this package is not properly installed
-    pkginfo = {}
-
-
-authors: str | list[str] = pkginfo.get("author_email", "unknown")
-
-license_: str | list[str] = pkginfo.get("license_expression") or pkginfo.get("license", "unknown") or "unknown"
-
-version: str = pkginfo.get("version", "unknown")  # type: ignore[assignment]
+version: str = importlib.metadata.version(PACKAGE)
+license_: str = pkginfo["License-Expression"]
+authors: str = pkginfo["Author-email"]
